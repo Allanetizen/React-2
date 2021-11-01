54364a658470fefdb6d5f42aaf08f3bddd897424
allanetizenm <allankosgei747@gmail.com>
2021-11-01 16:17:49 +0300

    arrow functions

--- Destructuring/file.js
+++ Destructuring/file.js
@@ -29,13 +29,55 @@ a = [1, 2, 3];
 b = [4, 5, 6];
 c = a.concat(b);
 console.log("c: " + c);
-//--------//
+//---/spread operator-----//
 b = [4, 5, 6];
-c = [...a, ...b]; //spread operator
+c = [...a, ...b]; //the spread operator
 console.log("c: " + c);
+
 //final spread operator
 const person = { name: "Jhon"};
 const student = { ID: "21", GPA: "3.0"};
 
 const new_object = { ...person, ...student, semester: '3'};
 console.log(new_object);
+
+//---------Arrow functions with Hooks---------\\
+// JavaScript ES5 function
+function getGreetingFunc() {
+    return 'Welcome to JavaScript';
+  }
+  
+  // JavaScript ES6 arrow function with body
+  const getGreetingArrow1 = () => {
+    return 'Welcome to JavaScript';
+  }
+  
+  // JavaScript ES6 arrow function without body and implicit return
+  const getGreetingArrow2 = () =>
+    'Welcome to JavaScript';
+  
+  console.log(getGreetingFunc());
+  console.log(getGreetingArrow1());
+  console.log(getGreetingArrow2());
+
+  //without arrow function
+  //---------------------const students = [
+    const students = [
+        { ID: 1, present: true},
+        { ID: 2, present: true},
+        { ID: 3, present: false}, 
+      ];
+      
+      const presentStudents = students.filter(function(student){return student.present;});
+      console.log(presentStudents);
+
+
+ //with arrow function
+ const students = [
+    { ID: 1, present: true},
+    { ID: 2, present: true},
+    { ID: 3, present: false}, 
+  ];
+  
+  const presentStudents = students.filter(student => student.present);
+  console.log(presentStudents);
